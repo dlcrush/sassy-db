246a221a6dc0a6c4a58b26f28018e4dd128bc8f4
David Crush <dlcrush@mail.roanoke.edu>
2017-01-26 21:32:19 -0500

    wip

--- src/main.js
+++ src/main.js
@@ -13,9 +13,46 @@ AWS.config.update({
 
 var dynamodb = new AWS.DynamoDB();
 
-/*
+var createTable = function(table) {
+  var params = {
+    TableName: table.name,
+    KeyShema: table.keys,
+    AttributeDefinitions: table.attributes
+  }
+
+  dynamodb.createTable(params, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+  });
+}
+
+var insert = function(table, items) {
+  itmes.forEach(function(item) {
+      var params = {
+          TableName: table,
+          Item: item
+      };
+
+      docClient.put({ TableName: table, Item: item }, function(err, data) {
+         if (err) {
+             console.error("Unable to add item", item, ". Error JSON:", JSON.stringify(err, null, 2));
+         } else {
+             console.log("PutItem succeeded:", item);
+         }
+      });
+  });
+}
+
+var query = function(table, query) {
+  
+}
+
+
 var params = {
-    TableName : "Movies",
+    TableName : "test",
     KeySchema: [
         { AttributeName: "year", KeyType: "HASH"},  //Partition key
         { AttributeName: "title", KeyType: "RANGE" }  //Sort key
@@ -23,11 +60,7 @@ var params = {
     AttributeDefinitions: [
         { AttributeName: "year", AttributeType: "N" },
         { AttributeName: "title", AttributeType: "S" }
-    ],
-    ProvisionedThroughput: {
-        ReadCapacityUnits: 10,
-        WriteCapacityUnits: 10
-    }
+    ]
 };
 
 dynamodb.createTable(params, function(err, data) {
@@ -36,7 +69,7 @@ dynamodb.createTable(params, function(err, data) {
     } else {
         console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
     }
-});*/
+});
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 
